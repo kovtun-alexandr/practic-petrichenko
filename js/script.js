26a04a923df4829fae762829806be8fc522367de
Alexander <kovtunalexander.84@gmail.com>
2022-04-24 20:58:18 +0300

    add object

--- js/script.js
+++ js/script.js
@@ -24,3 +24,11 @@
 'use strict';
 
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
