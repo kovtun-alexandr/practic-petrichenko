06ad74839f476bad46db4b610a163de87afb6978
Alexander <kovtunalexander.84@gmail.com>
2022-04-25 12:19:12 +0300

    add two variable

--- js/script.js
+++ js/script.js
@@ -33,10 +33,12 @@ const personalMovieDB = {
   privat: false
 };
 
-let lastViewed = prompt('Один из последних просмотренных фильмов?','');
-let rating = prompt('На сколько оцените его?','');
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies["lastViewed"] = lastViewed;
-personalMovieDB.movies['rating'] = rating;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
