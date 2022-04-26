74f3e2dfcb6d50e755c27d9f7d0b23cd64fcc2e8
Alexander <kovtunalexander.84@gmail.com>
2022-04-26 14:03:15 +0300

    redid the program using a loop

--- js/script.js
+++ js/script.js
@@ -33,12 +33,20 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных фильмов?',''),
-      d = prompt('На сколько оцените его?','');
+let count = 0;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+while (count < 2) {
+  const a = prompt('Один из последних просмотренных фильмов?',''),
+        b = prompt('На сколько оцените его?','');
+  
+  count++;
+  
+  personalMovieDB.movies[a] = b;
+}
+
+      // c = prompt('Один из последних просмотренных фильмов?',''),
+      // d = prompt('На сколько оцените его?','');
+
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
